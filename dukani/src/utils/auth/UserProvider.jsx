c539a7715f12b5831425382a24eaecf5b216feb4
briankintu <brian2xa@gmail.com>
2024-01-11 13:11:14 +0000

    january latestone

--- dukani/src/utils/auth/UserProvider.jsx
+++ dukani/src/utils/auth/UserProvider.jsx
@@ -23,7 +23,7 @@ const UserProvider = ({ children }) => {
       .then(() => {
         // Reload the page so that the boot info is fetched again
         const URL = import.meta.env.VITE_BASE_NAME ? `${import.meta.env.VITE_BASE_NAME}` : '';
-        window.location.replace(`${URL}/inga`);
+        window.location.replace(`/`);
         // window.location.reload()
       });
   };
