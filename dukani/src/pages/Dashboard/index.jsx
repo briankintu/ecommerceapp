315a72daa4665e60270f28becfbc4ee34ff77983
briankintu <brian2xa@gmail.com>
2024-01-11 15:32:41 +0300

    january last

--- dukani/src/pages/Dashboard/index.jsx
+++ dukani/src/pages/Dashboard/index.jsx
@@ -1,13 +1,60 @@
-import React from 'react'
-import {useState} from 'react'
+
 import Layout from '../../components/Layout'
-import Login from '../Auth/Login'
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import BreadcrumbCom from "../../components/BreadcrumbCom";
+
+
 
 const Dashboard = () => {
+  const [switchDashboard, setSwitchDashboard] = useState(false);
+  const location = useLocation();
+  const getHashContent = location.hash.split("#");
+  const [active, setActive] = useState("dashboard");
+  useEffect(() => {
+    setActive(
+      getHashContent && getHashContent.length > 1
+        ? getHashContent[1]
+        : "dashboard"
+    );
+  }, [getHashContent]);
     
   return (
-    <Layout>
-        <h1 className='text-black font-bold text-2xl'>Dashboard</h1>
+    <Layout childrenClasses="pt-0 pb-0">
+       <div className="profile-page-wrapper w-full">
+       <div className="container-x mx-auto">
+       <div className="w-full my-10">
+            <BreadcrumbCom
+              paths={[
+                { name: "home", path: "/" },
+                { name: "profile", path: "/profile" },
+              ]}
+            />
+            <div className="w-full bg-white px-10 py-9">
+            <div className="title-area w-full flex justify-between items-center">
+                <h1 className="text-[22px] font-bold text-qblack">
+                  Your Dashboard
+                </h1>
+                <div className="switch-dashboard flex space-x-3 items-center">
+                  <p className="text-qgray text-base">Switch Dashboard</p>
+                  <button
+                    onClick={() => setSwitchDashboard(!switchDashboard)}
+                    type="button"
+                    className="w-[73px] h-[31px] border border-[#D9D9D9] rounded-full relative "
+                  >
+                    <div
+                      className={`w-[23px] h-[23px] bg-qblack rounded-full absolute top-[3px] transition-all duration-300 ease-in-out ${
+                        switchDashboard ? "left-[44px]" : "left-[4px]"
+                      }`}
+                    ></div>
+                  </button>
+                </div>
+              </div>
+            </div>
+            </div>
+
+       </div>
+        </div>
     </Layout>
   )
 }
